05c7b409b50fe3368a6a15940f3773ba93d5ce7d
dharminder143 <singhdharminder118@gmail.com>
2020-08-21 10:43:03 +0530

    update jwt

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from blog import views
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 
 router = DefaultRouter()
 router.register('products', views.UserViewSet, basename='products-list')
@@ -8,4 +9,6 @@ router.register('register', views.UserCreate, basename='register')
 
 urlpatterns =[
 			path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+			path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    		path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]+router.urls
